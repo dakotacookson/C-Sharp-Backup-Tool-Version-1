0978119115d8ebcf78dcbc7aaded4aaedcacb097
DakotaCookson <dakota.cookson1995@gmail.com>
2021-07-07 23:08:58 -0700

    inital

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 200);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(193, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(583, 200);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(193, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(49, 238);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 58);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Source";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(614, 238);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 58);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Destination";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // folderBrowserDialog2
+            // 
+            this.folderBrowserDialog2.HelpRequest += new System.EventHandler(this.folderBrowserDialog2_HelpRequest);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.25F);
+            this.button3.Location = new System.Drawing.Point(295, 56);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(196, 118);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "File Transfer";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(36, 333);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(725, 82);
+            this.progressBar1.TabIndex = 5;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.Text = "Saved Backups";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Enabled = false;
+            this.panel1.Location = new System.Drawing.Point(92, 91);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(609, 253);
+            this.panel1.TabIndex = 7;
+            this.panel1.Visible = false;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(64, 89);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(466, 29);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(64, 144);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(198, 63);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Save Name";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(315, 144);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(200, 63);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "Run Backup";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(201, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 32);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Backup Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(550, 0);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(59, 29);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "Cancel";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public string sourceDir = "";
+        public string destinationDir = "";
+        public string fileName = "";
+        public string newdestdir = "";
+        public string backupname = "";
+
+        public static string UserName { get; }
+
+        string configPath = "C:/Users/" + Environment.UserName.ToString() + "/saved_backups.txt";
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (File.Exists(configPath))
+
+            {
+                foreach (string line in File.ReadLines(configPath))
+                {
+                    if (line.Contains("BackupName:"))
+                    {
+                        var lineset = line.Split('*');
+                        var lineset2 = lineset[1].ToString();
+                        comboBox1.Items.Add(lineset2);
+                    }
+                }
+
+            }
+            else
+            {
+                File.Create("C:/Users/" + Environment.UserName.ToString() + "/saved_backups.txt");
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+        private void folderBrowserDialog2_HelpRequest(object sender, EventArgs e)
+        {
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            sourceDir = textBox1.Text;
+            destinationDir = textBox2.Text;
+            if (sourceDir.Length >= 1 && destinationDir.Length >= 1)
+            {
+                panel1.Enabled = true;
+                panel1.Visible = true;
+
+                progressBar1.Value = 0;
+
+            }
+            else
+            {
+                MessageBox.Show("Please Enter Source and Destination Data");
+            }
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            if (File.Exists(configPath))
+
+            {
+                foreach (string line in File.ReadLines(configPath))
+                {
+                    if (line.Contains(backupname) && line.Contains("SourceName:"))
+                    {
+                        var lineset = line.Split('*');
+                        var lineset2 = lineset[1].ToString();
+                        textBox1.Text = lineset2;
+                    }
+
+                    if (line.Contains(backupname) && line.Contains("DestinationName:"))
+                    {
+                        var lineset = line.Split('*');
+                        var lineset2 = lineset[1].ToString();
+                        textBox2.Text = lineset2;
+                    }
+                    if (line.Contains(backupname) && line.Contains("BackupName:"))
+                    {
+                        var lineset = line.Split('*');
+                        var lineset2 = lineset[1].ToString();
+                        richTextBox1.Text = lineset2;
+                    }
+                }
+
+            }
+
+
+            sourceDir = textBox1.Text;
+            destinationDir = textBox2.Text;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            if (richTextBox1.Text.Length <= 0)
+            {
+                button4.Enabled = false;
+                button5.Enabled = false;
+            }
+            if (richTextBox1.Text.Length >= 1)
+            {
+                button4.Enabled = true;
+                button5.Enabled = true;
+            }
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text.Length <= 0)
+            {
+                button4.Enabled = false;
+                button5.Enabled = false;
+            }
+            if (richTextBox1.Text.Length >= 1)
+            {
+                button4.Enabled = true;
+                button5.Enabled = true;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            sourceDir = textBox1.Text;
+            destinationDir = textBox2.Text;
+            backupname = richTextBox1.Text;
+            using (StreamWriter sw = File.AppendText(configPath))
+                sw.WriteLine("BackupName: " + "*" + backupname);
+            using (StreamWriter sw = File.AppendText(configPath))
+                sw.WriteLine(backupname + "SourceName:" + "*" + sourceDir);
+            using (StreamWriter sw = File.AppendText(configPath))
+                sw.WriteLine(backupname + "DestinationName:" + "*" + destinationDir);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            panel1.Enabled = false;
+            panel1.Visible = false;
+
+            newdestdir = destinationDir + "/" + "temp";
+            Directory.CreateDirectory(newdestdir);
+            {
+                //Thread t = new Thread(new ThreadStart(NewMethod));
+                //t.Start();
+                //while (t.IsAlive)
+                //{
+                //    Application.DoEvents();
+                //    Thread.Sleep(100);
+                //}
+                //void NewMethod()
+                {
+                    foreach (string d in Directory.GetDirectories(sourceDir, "*",
+                        SearchOption.AllDirectories))
+                        Directory.CreateDirectory(d.Replace(sourceDir, newdestdir));
+
+                    //Copy all the files & Replaces any files with the same name
+                    foreach (string f in Directory.GetFiles(sourceDir, "*.*",
+                        SearchOption.AllDirectories))
+                        File.Copy(f, f.Replace(sourceDir, newdestdir), true);
+
+
+
+                    string startPath = destinationDir + "/" + "temp";
+                    string zipPath = destinationDir + "/" + backupname + ".zip";
+                    if (File.Exists(zipPath))
+                    {
+                        File.Delete(zipPath);
+                    }
+
+                    ZipFile.CreateFromDirectory(startPath, zipPath);
+                    Directory.Delete(newdestdir, true);
+                }
+                progressBar1.Value = 100;
+
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+            panel1.Enabled = false;
+        }
+    }
+}
+
